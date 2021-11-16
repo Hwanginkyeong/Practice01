4bbcab235feeb650cbf171046797d7ca1aa2400b
hwanginkyeong <inke0831@naver.com>
2021-11-16 23:01:49 +0900

    2nd

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -6,7 +6,11 @@ public class Ex20 {
 	
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("500원의 개수: ");
+		System.out.println("100원의 개수: ");
+		System.out.println("50원의 개수: ");
+		System.out.println("10원의 개수: ");
+		//System.out.println("동전의 총합은 " +  + "입니다.");
 		
 		
 		
