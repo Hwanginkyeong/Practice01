0b32884b2064d0c7c25e5c00311414ba8e224ff7
hwanginkyeong <inke0831@naver.com>
2021-11-22 10:24:35 +0900

    2nd

--- src/com/javaex/practice/Ex20.java
+++ src/com/javaex/practice/Ex20.java
@@ -6,11 +6,22 @@ public class Ex20 {
 	
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("500원의 개수: ");
-		System.out.println("100원의 개수: ");
-		System.out.println("50원의 개수: ");
-		System.out.println("10원의 개수: ");
-		//System.out.println("동전의 총합은 " +  + "입니다.");
+		
+		System.out.print("500원의 개수: ");
+		int won500 = sc.nextInt();
+		
+		System.out.print("100원의 개수: ");
+		int won100 = sc.nextInt();
+		
+		System.out.print("50원의 개수: ");
+		int won50 = sc.nextInt();
+		System.out.print("10원의 개수: ");
+		int won10 = sc.nextInt();
+		
+		int sum = 500*won500+100*won100+50*won50+10*won10;
+		
+		
+		System.out.println("동전의 총합은 " + sum + "원 입니다.");
 		
 		
 		
